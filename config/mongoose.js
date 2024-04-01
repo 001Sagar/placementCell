ebf56a026a95b9601bb2b92655acf02ede6b768e
001Sagar <109284668+001Sagar@users.noreply.github.com>
2024-04-01 09:49:54 +0530

    save

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 async function main(){
-    await mongoose.connect('mongodb+srv://placement:1UbhMXi13RXuCwSn@cluster0.p5gbb.mongodb.net/placementcell?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://Sagar:Sagar@9760@cluster0.ozi7tg0.mongodb.net/');
     console.log("connection Successfull !! ");
 }
 main().catch(error =>console.log("connection not successfull !!"));
